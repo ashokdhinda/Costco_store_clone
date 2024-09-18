f50c6a8e8ada7e322078d8e50b1988be9e0744e7
ashokdhinda <ashokkudhinda143@gmail.com>
2024-09-19 00:03:58 +0530

    done

--- my-Cosctc-app/src/Redux/actionItems.js
+++
@@ -1,7 +0,0 @@
-export const Email = "email";
-export const Password = "password";
-export const Signup_Success = "signup_success";
-export const Authentication = "authentication";
-export const logout = "logout";
-export const LOADING = "LOADING"
-export const IS_LOADING = "IS_LOADING"
