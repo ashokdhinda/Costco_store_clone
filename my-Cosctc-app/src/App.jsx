0d7d69e8d9122715d20495248ea9766a20304f5d
ashokdhinda <ashokkudhinda143@gmail.com>
2024-09-18 14:06:19 +0530

    header part completed

---
+++ my-Cosctc-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ my-Cosctc-app/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import Header from '../Components/Header'
+
+const Home = () => {
+  return (
+  
+      <Box>
+        <Header />
+      </Box>
+    
+  );
+}
+
+export default Home
